ebe40eeb07cf446c96ecddf735813fe1393d03a0
ice <eriklarsson02@gmail.com>
2025-06-24 12:50:05 +0200

    Startup

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 import express from 'express';
 import morgan from 'morgan';
 import mongoose from 'mongoose';
+import ejs from 'ejs';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const app = express();
+app.set('view engine', 'ejs');
+app.use(express.json());
+app.use(morgan('dev'));
+
+app.get('/', (req, res) => {
+    res.render('index');
+})
+
+app.get('/download', (req, res) => {
+    res.download('./public/css/style.css');
+})
+
+app.listen(process.env.PORT, () => {
+    console.log(`Listening on port ${process.env.PORT}`);
+})
