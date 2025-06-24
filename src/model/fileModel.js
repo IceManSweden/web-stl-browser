84ba061468585d6005f743d1df60f8a1b209a8d1
ice <eriklarsson02@gmail.com>
2025-06-24 22:02:37 +0200

    Added comments

--- src/model/fileModel.js
+++ src/model/fileModel.js
@@ -11,6 +11,13 @@ const File = new mongoose.model("File", schema);
 const FileModel = {}
 export default FileModel;
 
+/**
+ * Creates a new model.
+ * @param name The models name
+ * @param filePath the path for the model.
+ * @param imagePath the path for the image.
+ * @returns Object if it was successful
+ */
 FileModel.create = async (name, filePath, imagePath) => {
     const f = new File();
     f.name = name;
@@ -20,15 +27,26 @@ FileModel.create = async (name, filePath, imagePath) => {
         return false
     });
 }
-
+/**
+ * Finds the model by name.
+ * @param name the name of the model.
+ * @returns Object the model by name.
+ */
 FileModel.findByName = async (name) => {
     return File.find({name: name});
 }
-
+/**
+ * Gets all the models.
+ * @returns Object the model's info.
+ */
 FileModel.getAll = async () => {
     return File.find();
 }
-
+/**
+ * Finds and get the model info by id.
+ * @param id the id of the model.
+ * @returns Object the model.
+ */
 FileModel.findById = async (id)=>{
     return File.findById(id)
 }
